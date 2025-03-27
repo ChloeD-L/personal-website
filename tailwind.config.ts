7baa2f016a07871ee5f918b5a4b7218d369ee244
ChloeD-L <linduan1018@gmail.com>
2025-03-27 13:00:55 +1100

    update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,15 +14,32 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
-        'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        "ping-slow": "ping 3s cubic-bezier(0, 0, 0.2, 1) infinite",
+        "pulse-slow": "pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
       keyframes: {
         ping: {
-          '75%, 100%': { transform: 'scale(2)', opacity: '0' },
+          "75%, 100%": { transform: "scale(2)", opacity: "0" },
         },
         pulse: {
-          '50%': { opacity: '.5' },
+          "50%": { opacity: ".5" },
+        },
+      },
+      colors: {
+        primary: {
+          light: "#606c38",
+          DEFAULT: "#283618",
+          dark: "#141b0c",
+        },
+        secondary: {
+          light: "#dda15e",
+          DEFAULT: "#bc6c25",
+          dark: "#8c4c15",
+        },
+        background: {
+          light: "#fefae0",
+          DEFAULT: "#e9edc9",
+          dark: "#ccd5ae",
         },
       },
     },
